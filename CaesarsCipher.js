5755ec46b8b7f5854288b71fa8dfcca9b74ab36b
DivSriv <72649014+DivSriv@users.noreply.github.com>
2022-03-04 10:23:50 +0530

    Create CaesarsCipher.js

---
+++ CaesarsCipher.js
@@ -0,0 +1,22 @@
+function rot13(str) {
+  let regex = /[A-Z]/
+  str = str.split("")
+  let store =0;
+  for(let i=0; i<str.length; i++){
+    if(regex.test(str[i])){
+      console.log(str[i].charCodeAt())
+      store = 65 + str[i].charCodeAt() % 26;
+      console.log(store)
+      str[i] = String.fromCharCode(store);
+      console.log(str[i])
+    }
+  }
+  str = str.join("");
+  console.log(str)
+  return str;
+}
+
+rot13("SERR PBQR PNZC");
+rot13("SERR CVMMN!");
+rot13("SERR YBIR?");
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.");
